76a658060585e542c9110159901148253a3a6345
Fernando <nerifernando26@gmail.com>
2021-03-30 22:44:16 -0300

    Criacao das Entidades

---
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+        
+        public ICollection<Pedido> Pedidos { get; set; } // O usuário pode ter um ou mais pedidos
+    }
+}
